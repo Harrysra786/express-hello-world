2444a928ffb2afb94ae4439dc32abefbd9f5ff8d
Harrysra786 <124359506+Harrysra786@users.noreply.github.com>
2023-07-22 00:09:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const { JSDOM } = require('jsdom');
-const { createCanvas } = require('canvas');
+const puppeteer = require('puppeteer');
 const Diff = require('diff');
 
 const app = express();
@@ -15,30 +14,30 @@ app.post('/compare', async (req, res) => {
   const paragraphs1 = text1.split('\n');
   const paragraphs2 = text2.split('\n');
 
-  const dom = new JSDOM(`<!DOCTYPE html><body><div id="diffOutput"></div></body>`);
-  const outputDiv = dom.window.document.getElementById('diffOutput');
-
+  let html = '<div id="diffOutput">';
   for (let i = 0; i < paragraphs1.length; i++) {
     const diff = Diff.diffWords(paragraphs1[i], paragraphs2[i]);
     diff.forEach(part => {
-      const span = dom.window.document.createElement('span');
-      span.textContent = part.value;
       if (part.added) {
-        span.classList.add('added');
+        html += `<span class="added">${part.value}</span>`;
       } else if (part.removed) {
-        span.classList.add('removed');
+        html += `<span class="removed">${part.value}</span>`;
+      } else {
+        html += `<span>${part.value}</span>`;
       }
-      outputDiv.appendChild(span);
     });
-    outputDiv.appendChild(dom.window.document.createElement('br'));
+    html += '<br>';
   }
+  html += '</div>';
 
-  const canvas = createCanvas(800, 600);
-  const ctx = canvas.getContext('2d');
-  ctx.fillText(outputDiv.innerHTML, 50, 50);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(html);
+  const element = await page.$('#diffOutput');
+  const screenshot = await element.screenshot({ type: 'png' });
+  await browser.close();
 
-  const buffer = canvas.toBuffer('image/png');
-  res.type('image/png').send(buffer);
+  res.type('image/png').send(screenshot);
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
